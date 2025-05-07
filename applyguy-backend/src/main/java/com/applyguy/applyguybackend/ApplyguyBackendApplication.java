f0f0e65ee493cbfc9b57463ee45f737f7a6915ff
aayushgarg19 <aayushgarg.iitb@gmail.com>
2025-05-07 18:55:24 +0530

    Initial commit

---
+++ applyguy-backend/src/main/java/com/applyguy/applyguybackend/ApplyguyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.applyguy.applyguybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyguyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyguyBackendApplication.class, args);
+	}
+
+}
---
+++ applyguy-backend/src/test/java/com/applyguy/applyguybackend/ApplyguyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applyguy.applyguybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyguyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
